09188719ded7c3eda2d596619c6cc684ec92694c
Hakan OZGUR <hakan.ozgur@gmail.com>
2023-12-15 18:29:53 +0300

    ..

--- uix_demo.py
+++ uix_demo.py
@@ -2,6 +2,10 @@ import uix
 from uix.core.element import Element
 # Main Application Instance
 app = uix.uix_app
-#ui = Element()
-app.start( debug = True)
+
+def main():
+    ui = Element("Hello World")
+    return ui
+
+app.start(ui = main, debug = True)
 
