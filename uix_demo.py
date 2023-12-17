585b0a588a05ee6602352f5e80473c69a947ab85
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2023-12-17 21:04:17 +0300

    ..

--- uix_demo.py
+++ uix_demo.py
@@ -1,12 +1,29 @@
 import uix
-from uix.elements import div, button
-# Main Application Instance
+from uix.elements import div, button, grid
+counter = 0
+def next_counter():
+    global counter
+    counter = counter + 1
+    return counter
 
-def main():
-    with div("main") as ui:
-        div("Hello World")
-        button("Hello World")
-    return ui
+def on_click(ctx, id, value):
+    print("Clicked", id, value)
+    ctx.element.value = "Clicked!" + str(next_counter())
 
-uix.start(ui = main, debug = True)
+def button1(value):
+    button(value).on("click",on_click)
 
+def comp1():
+    with grid("",columns= "40% 1fr") as grid1:
+        grid1.style("width","400px")
+        button1("A!")
+        button1("B!")
+        button1("C!")
+        button1("D!")
+
+
+with div("Example") as main:
+    comp1()
+
+print(main.render())
+uix.start(ui = main,debug=True)
