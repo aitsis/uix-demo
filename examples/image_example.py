0af3f2cfc717dfc717081abfc8c5651e8fed2409
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2023-12-24 16:29:15 +0300

    clean code example

--- examples/image_example.py
+++ examples/image_example.py
@@ -1,5 +1,4 @@
-from uix.elements import image
-import os
+from uix.elements import image # type: ignore
 def image_example():
     image_url = "https://ai.ait.com.tr/wp-content/uploads/AIT_AI_LOGO.png"
     main = image(image_url).cls("image")
--- uix_demo.py
+++ uix_demo.py
@@ -1,33 +1,41 @@
 import uix
 import os
 import importlib
+from threading import Timer
 from uix.elements import div, grid, container, md # type: ignore
 from _menu import menu
-examples = {}
+from uix.pipes import status_pipe
+
 readme = open("README.md").read()
 
-for file_name in os.listdir("examples"):
-    if file_name.endswith(".py"):
-        module = importlib.import_module(f"examples.{file_name[:-3]}")
-        examples[file_name[:-11]] = {"module":module,"name":module.__name__}
-        
+def get_examples_from_examples_folder():
+    examples = {}
+    for file_name in os.listdir("examples"):
+        if file_name.endswith(".py"):
+            module = importlib.import_module(f"examples.{file_name[:-3]}")
+            examples[file_name[:-11]] = {"module":module,"name":module.__name__}
+    return examples
+            
+examples = get_examples_from_examples_folder()
 
 uix.html.add_css_file("uix_demo.css")
 
-def chooseExample(ctx, id, value):
-    print("Choose example", value)
+def get_example(name):
+    return getattr(examples[name]["module"], name+"_example")()
+
+def updateExample(ctx, id, value):
     content = ctx.elements["content"]
     with content:
-        getattr(examples[value]["module"], value+"_example")()
+        get_example(value)
     content.update()
             
 with div("") as page:
     with grid("",columns = "150px 600px") as main:
         main.cls("main")
-        menu(chooseExample,examples.keys())
+        menu(updateExample,examples.keys())
         with container("",id ="content") as content:
             content.cls("content border")
             md(readme)
             
         
-uix.start(ui = page,config = {"debug" : True})
+uix.start(ui = page,config = {"debug" : True, "pipes":[status_pipe()]})
