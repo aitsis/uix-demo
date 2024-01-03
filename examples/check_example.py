89a2f0884a2d9f3038b2b3510bad8d6efb83c7ef
semiharslanait <semih@ait.com.tr>
2024-01-03 11:42:22 +0300

    Add check_example.py

---
+++ examples/check_example.py
@@ -0,0 +1,16 @@
+from uix.elements._check import check  # Assuming check is the class within _check module
+from uix.elements._check import title, description, sample as code
+from uix.elements._div import div
+
+def check_example():
+    main = check("", id="check").on("click", on_click)
+    test = div("test", "test")
+    return main
+
+def on_click(ctx, id, value):
+    print("Clicked", id, value)
+    if value == False:
+        ctx.elements["test"].value = "Unchecked!"
+    else:
+        ctx.elements["test"].value = "Checked!"
+    ctx.elements["test"].update()
