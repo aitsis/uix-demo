21e228b8cf48b7ef4c2eb21214fc640da32e3374
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2024-01-09 20:24:30 +0300

    file_example updated.

--- examples/file_example.py
+++ examples/file_example.py
@@ -1,61 +1,65 @@
+import uix
 
-from uix.elements import div, col, border, file, image, container, dialog, button, row, check
-from uix.app import files
-import io
-from PIL import Image
-def on_file_upload_events(ctx,id,value):
-    url = value["url"]
-    status = value["status"]
-    print("url = ",url," status = ",status)
+from uix.elements import file, div, image, col, row, progress
 
-    if status == "error":
-        print("Error = ", value["error"])
-    else:
-        data = files[url]["data"]
-        print("File size = ", len(data))
-        image = Image.open(io.BytesIO(data))
-        grayimage = image.convert("L")
-        files[url] = None
-        newurl = url[-14:]
-        print("newurl = ",newurl)
-        temp = io.BytesIO()
-        grayimage.save(temp,format="png")
-        files[newurl] = {"data":temp.getvalue(),"type":"image/png"}        
-        #ctx.elements[url].value = "/download/"+newurl
-        ctx.elements["dialog-image"].value = "/download/"+newurl
+def lineitem(value):
+    div(value).style("padding","5px;")
 
+def upload_file(ctx, id, value):
+    print("upload_file",id)
+    ctx.elements["file"].upload(id)
     
 
-def on_click_image_upload(ctx,id,value):
-    print("image upload Clicked image = ",value)
-    elm = ctx.elements["myFile"]
-    print("elm = ",elm)
-    ctx.elements["myFile"].upload(value)
+def line(file):
+    with row(id=file.url).style("margin-top","10px").on("click", upload_file) as r:
+        image(value=file.url).size(None,100)
+        lineitem(file.name)
+        lineitem(file.size)
+        lineitem(file.type)
+        progress(id = file.url+"_progress")
+            
 
-def on_click_image_show_dialog(ctx,id,value):
-    print("Clicked image22 = ",value) 
-    with ctx.elements["image-dialog"]:
-        image(value,id = "dialog-image").style("width","100%").style("height","100%").on("click",lambda ctx,id,value: ctx.elements["image-dialog"].hide() )
-        button("Upload File",id = "btn"+value).on("click",lambda ctx,id,value: ctx.elements["myFile"].upload(id[3:]))
-    ctx.elements["image-dialog"].update()
-    ctx.elements["image-dialog"].show()
+def list_files(ctx, files):
+    with ctx.elements["files"]:
+        for file in files:
+            line(file)
+    ctx.elements["files"].update()
 
-def on_change(ctx,id,value):
-    print("File = ",value," id = ",id)
-    with ctx.elements["images"]:
-        width = "300px"
-        if len(value) > 1:
-            width = "100px"
-        for i in range(len(value)):
-            (image(value[i],id=value[i]).style("margin-top","20px")
-             .style("margin-left","5px").style("width",width).on("click",on_click_image_show_dialog))
-    ctx.elements["images"].update()
+def show_error(ctx, error):
+    with ctx.elements["files"]:
+        div(error).style("color","red")
+    ctx.elements["files"].update()
+
+
+def select_callback(ctx, id, data, status):
+    if status == "done":
+        list_files(ctx, data)
+    else:
+        show_error(ctx, data)
+
+def upload_callback(ctx, id, data, status):
+    if status == "done":
+        print("upload done",len(data))
+        #>>>>>>>>>>>>>>>>>>>
+        # USE DATA HERE
+        #<<<<<<<<<<<<<<<<<<<<
+    elif status == "progress":
+        ctx.elements[data["url"] + "_progress"].value = data["progress"]
+    else:
+        show_error(ctx, data)
+
+def file_callback(ctx, id, event, data, status):
+    if event == "select":
+        select_callback(ctx, id, data, status)    
+    elif event == "upload":
+        upload_callback(ctx, id, data, status)
+
 
 def file_example():
-    with container("") as main:
-        with border(""):
-            file("",id="myFile",multiple = True).on("change",on_change).on("file-upload",on_file_upload_events)
-            check("multiple",id="multiple").on("change",lambda ctx,id,value: ctx.elements["myFile"].set_attr("multiple",value))
-        div("",id = "images").style("max-height","600px").style("overflow","auto")
-        dialog("","image-dialog")
-    return main
+    with div(id="root").style("max-height","80%").style("overflow","auto") as root:
+        file(id="file", multiple=True,callback = file_callback, accept="image/png,image/jpeg,image/gif")
+        div("Files:", id="files").style("margin-top","10px")
+    return root
+
+
+         
