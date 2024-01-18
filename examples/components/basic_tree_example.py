2cbf5f487bdcac224cd445bb7a2a20bf7e0d000e
mugegngrd <muge@ait.com.tr>
2024-01-18 09:22:19 +0300

    Add basic_tree_example.py to examples/components

---
+++ examples/components/basic_tree_example.py
@@ -0,0 +1,24 @@
+import uix
+from uix.elements import div
+from uix_components import basic_tree_view
+from uix_components._basic_tree_view._basic_tree_view import title, description, sample as code
+
+
+data = {
+    "key1": "value1",
+    "key2": "value2",
+    "key3": {
+        "key4": "value4",
+        "key5": "value5",
+        "key6": {
+            "key7": "value7",
+            "key8": "value8",
+            "key9": "value9",
+        }
+    }
+}
+
+def basic_tree_example():
+    with div() as basic_tree_view_example:
+        basic_tree_view(id="basic_tree_view_example",data=data,main_title="views")
+    return basic_tree_view_example
