0b58e7853b2ed550e342b8998f59038b8d632d98
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2024-01-24 16:01:37 +0300

    fabric_test

--- uix_demo2.py
+++ uix_demo2.py
@@ -1,123 +1,12 @@
 import uix
-from uix.elements import border, md
-from uix_components import chart
-print("Imported: chart_example")
 
-chart_value = {
-            "type": "line",
-            "data": {
-            "labels": ["January", "February", "March", "April", "May", "June", "July"],
-            "datasets": [
-                {
-                    "label": "My First dataset",
-                    "backgroundColor": "rgba(255,99,132,0.2)",
-                    "borderColor": "rgba(255,199,132,1)",
-                    "borderWidth": 2,
-                    "hoverBackgroundColor": "rgba(255,99,132,0.4)",
-                    "hoverBorderColor": "rgba(255,99,132,1)",
-                    "data": [65, 59, 80, 81, 56, 55, 40],
-                }
-            ],
-        },
-        "options": {
-            "responsive": True,
-            "plugins": {
-                "legend": {
-                "position": 'top',
-                },
-            "title": {
-            "display": True,
-            "text": 'Example Chart'
-                }
-            }
-        }
-    }
-import pprint as pp
+from uix.elements import container,div, border
 
+from uix_components import fabric
 
-chart_str = pp.pformat(chart_value)
-print(isinstance(chart_str,str))
-print(chart_str)
-
-
-descriptions = [
-"""
------
-# chart
-## 1 - Temel Chart Kullanımı
-## 2 - Basit Chart Kullanımı
-----
-## 1 - Temel Chart Kullanımı
-Bu kullanımda chart componenti oluşturulur ve value değişkeni ile chart'ın tüm özellikleri belirlenir.
-
-Bu en temel kullanım şeklidir.
-
-[https://www.chartjs.org/docs/latest/](https://www.chartjs.org/docs/latest/) adresinden chart'ın tüm özelliklerine ulaşabilirsiniz.
-
-Temel kullanımda aşağıdaki gibi tüm veri seti belirlenir:
-```python
-chart_value = {
-            "type": "line",
-            "data": {
-            "labels": ["January", "February", "March", "April", "May", "June", "July"],
-            "datasets": [
-                {
-                    "label": "My First dataset",
-                    "backgroundColor": "rgba(255,99,132,0.2)",
-                    "borderColor": "rgba(255,99,132,1)",
-                    "borderWidth": 1,
-                    "hoverBackgroundColor": "rgba(255,99,132,0.4)",
-                    "hoverBorderColor": "rgba(255,99,132,1)",
-                    "data": [65, 59, 80, 81, 56, 55, 40],
-                }
-            ],
-        }}
-```
-Ardından chart componenti oluşturulur:
-```python
-
-from uix_components import chart
-
- chart1 = chart(id="chart1",value= chart_value).size("100%","500px")
-
-```
-"""
-,
-"""
-## 2 - Basit Chart Kullanımı
-Bu kullanımda chart componenti oluşturulur ve value değişkeni ile chart'ın tüm özellikleri belirlenir.
-
-Bu en temel kullanım şeklidir.
-
-
-
-
-
-"""
-]
-
-import numpy as np
-test_data = np.random.randint(0, 100, size=(2, 100))
-print(test_data)
-
-def chart_example():
-    with uix.elements.border().size("100%","100%").style("overflow-y","auto") as main:
-        md(descriptions[0])
-        c1 = chart(id="chart1",type="line",value= chart_value).size("100%","500px")
-        c1.attrs["height"] ="500px"
-        md(descriptions[1])
+def main():
+    with border() as main: 
+        fabric(id="fabric1", width=1000, height=1000).cls("center")
     return main
 
-import inspect
-strsource = inspect.getsource(chart_example)
-print(strsource)
-
-uix.start(ui = chart_example(), config={"debug":True})
-
-
-# TODO
-
-# eklenecek elementler
-# !? meter
-# object ???
-
+uix.start(ui=main(), config={"debug": True})
