706605a2eb61a73a42ed848857cf0fd0111f9702
semiharslanait <semih@ait.com.tr>
2024-01-30 10:17:05 +0300

    Refactor alert_example.py to use row instead of col and add multiple alert types

--- examples/components/alert_example.py
+++ examples/components/alert_example.py
@@ -1,11 +1,16 @@
-import uix
-import uix_components
-from uix.elements import div, button, grid, col
+from uix.elements import button, row
 from uix_components import basic_alert
 from uix_components._basic_alert._basic_alert import title, description, sample as code 
 
 def alert_example():
-    with col() as content:
-        alert = basic_alert("", id = "myAlert", type="success")
-        button("Show Alert", id = "show_alert").on("click", lambda ctx, id, value: alert.open("alert-success", "selam"))
+    with row() as content:
+        alertSuccess = basic_alert("", id = "myAlert", type="success")
+        alertWarning = basic_alert("", id = "myAlertWarning", type="warning")
+        alertDanger = basic_alert("", id = "myAlertDanger", type="danger")
+        alertInfo = basic_alert("", id = "myAlertInfo", type="info")
+        button("Show Alert Danger").on("click", lambda ctx, id, value: alertDanger.open("alert-danger", "selam")).cls("alert-danger")
+        button("Show Alert Warning").on("click", lambda ctx, id, value: alertWarning.open("alert-warning", "selam")).cls("alert-warning")
+        button("Show Alert Info").on("click", lambda ctx, id, value: alertInfo.open("alert-info", "selam")).cls("alert-info")
+        button("Show Alert Success").on("click", lambda ctx, id, value: alertSuccess.open("alert-success", "selam")).cls("alert-success")
+    
     return content
