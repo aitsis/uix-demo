e8a393c4fb19f07f430eb1432ae7da3365eb309f
semiharslanait <semih@ait.com.tr>
2024-01-30 17:14:55 +0300

    Refactor alert_example.py

--- examples/components/alert_example.py
+++ examples/components/alert_example.py
@@ -3,14 +3,12 @@ from uix_components import basic_alert
 from uix_components._basic_alert._basic_alert import title, description, sample as code 
 
 def alert_example():
-    with row() as content:
-        alertSuccess = basic_alert("", id = "myAlert", type="success")
-        alertWarning = basic_alert("", id = "myAlertWarning", type="warning")
-        alertDanger = basic_alert("", id = "myAlertDanger", type="danger")
-        alertInfo = basic_alert("", id = "myAlertInfo", type="info")
-        button("Show Alert Danger").on("click", lambda ctx, id, value: alertDanger.open("alert-danger", "selam")).cls("alert-danger")
-        button("Show Alert Warning").on("click", lambda ctx, id, value: alertWarning.open("alert-warning", "selam")).cls("alert-warning")
-        button("Show Alert Info").on("click", lambda ctx, id, value: alertInfo.open("alert-info", "selam")).cls("alert-info")
-        button("Show Alert Success").on("click", lambda ctx, id, value: alertSuccess.open("alert-success", "selam")).cls("alert-success")
+    with row().style("gap","10px") as content:
+        basic_alert_el = basic_alert("", id = "comp_alert")
+
+        button("Show Alert Danger With Icon").on("click", lambda ctx, id, value: basic_alert_el.open("alert-danger", "selam", "fa-solid fa-circle-xmark")).cls("alert-danger")
+        button("Show Alert Warning").on("click", lambda ctx, id, value: basic_alert_el.open("alert-warning", "selam")).cls("alert-warning")
+        button("Show Alert Info").on("click", lambda ctx, id, value: basic_alert_el.open("alert-info", "selam merhaba ben umut savaşım çeliker")).cls("alert-info")
+        button("Show Alert Success").on("click", lambda ctx, id, value: basic_alert_el.open("alert-success", "selam")).cls("alert-success")
     
     return content
