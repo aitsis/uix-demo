96d9b11bd9e6a0a4362d1dea0f046e4ca6fcd509
Emre <emre@ait.com.tr>
2024-01-31 15:28:20 +0300

    Add form submission functionality to form_example.py

--- examples/form_example.py
+++ examples/form_example.py
@@ -1,26 +1,32 @@
-from uix.elements import form, div, button, label, input, grid, col
+from uix.elements import form, div, button, label, input, grid, col, textarea
 from uix.elements._form import title, description, sample as code
 
-def comp1():
-    with form(id="myForm") as form1:
-        form1.cls("border")
-        with grid("",columns="1fr 4fr") as grid1:
-            grid1.style("gap","10px")
-            grid1.style("padding","10px")
-            grid1.style("width","300px")
-            with col().style("font-size","20px"):
-                label("Name", usefor="name")
-                label("Email", usefor="email").style("margin-top","10px")
-            with col():
-                input("", id="name",  placeholder="Enter your name")
-                input("", id="email",  placeholder="Enter your email").style("margin-top","10px")
-        
-            button("Submit").style("margin-top","10px")
-       
+def submit_form(ctx, id, value):
+    print("Form submitted")
+    form = {
+        "name": ctx.elements["name"].value,
+        "email": ctx.elements["email"].value
+        }
+    print(form)
+    
+def input_setter(ctx, id, value):
+    ctx.elements[id].value = value
         
 def form_example():
     with div("") as main:
-        comp1()
+        with form(id="myForm").cls("border").on("submit", submit_form):
+            with grid("",columns="1fr 4fr") as grid1:
+                grid1.style("gap","10px")
+                grid1.style("padding","10px")
+                grid1.style("width","300px")
+                with col().style("font-size","20px"):
+                    label("Name", usefor="name")
+                    label("Email", usefor="email").style("margin-top","10px")
+                with col():
+                    input("", id="name",  placeholder="Enter your name").on("change", input_setter).style("margin-top","10px")
+                    input("", id="email",  placeholder="Enter your email").on("change", input_setter).style("margin-top","10px")
+            
+                button("Submit", type="submit").style("margin-top","10px")
     return main
 
 
