513493349372794acc82bffc5aa8813f791fe113
Mert <mert@ait.com.tr>
2024-05-07 16:29:52 +0300

    Add select_label callback to tree_view_example

--- examples/components/tree_view_example.py
+++ examples/components/tree_view_example.py
@@ -1,16 +1,21 @@
 from uix_components._tree_view._tree_view import title, description, sample as code 
 from uix_components import tree_view
+from uix.elements import text, div
 
 data = {
     "Examples": {
-        "Elements": ["Jupiter", "Saturn"], 
-        "Components": ["Uranus", "Neptune"],
         "Styles": {
             "Colors": ["Red", "Green"],
             "Shapes": ["Circle", "Square"]
-        }
+        },
+        "Components": ["Select", "Tree View"]
     }
  }
 
+def select_label(ctx, id, value):
+    ctx.elements["output"].value = f'Selected Value: {value}'
+
 def tree_view_example():
-    tree_view(id="tree_view_example",data=data)
+    with div().cls("gap"):
+        tree_view(id="tree_view_example",data=data, callback= select_label)
+        text("Selected Value:", id="output")
