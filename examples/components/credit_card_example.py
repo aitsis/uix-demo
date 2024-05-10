03e08550858c3e26714ca89f1993bf97c603eafb
Mert <mert@ait.com.tr>
2024-05-10 18:09:33 +0300

    credit_card example css update

--- examples/components/credit_card_example.py
+++ examples/components/credit_card_example.py
@@ -1,12 +1,8 @@
-import uix
-import os
 from uix_components import credit_card
 from uix_components._credit_card._credit_card import title, description, sample as code
-from uix.elements import div,image
-
-
+from uix.elements import div
 
 def credit_card_example():
-    with div("").size("60%","50%") as credit_card_example:
+    with div().size("100%","100%") as credit_card_example:
         credit_card("",id="credit_card_example")
     return credit_card_example
