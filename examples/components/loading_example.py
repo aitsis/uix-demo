f0f8a44cba6605f7cac759110c96ad1707fb717a
Mert <mert@ait.com.tr>
2024-05-13 17:01:31 +0300

    example and description update

--- examples/components/loading_example.py
+++ examples/components/loading_example.py
@@ -1,12 +1,9 @@
-import uix
-import uix_components
-
 from uix.elements import button, col
 from uix_components import basic_loading
 
 def loading_example():
     with col() as content:
-        loading = basic_loading(id = "myLoading").style("height","400px")
+        loading = basic_loading(id = "myLoading", timer = 5).style("height","400px")
         button("Start Loading", id = "show_loading").on("click", lambda ctx, id, value : loading.start())
         button("Stop Loading", id = "hide_loading").on("click", lambda ctx, id, value : loading.stop())
     return content
@@ -14,19 +11,24 @@ def loading_example():
 title="Loading Example"
 
 description = '''
-    Loading example shows how to create a loading component.
+## basic_loading(id, timer)
+1. Temel loading elementi.
+2. Timer parametresi ile loading süresi belirlenebilir. Varsayılan değeri 10 saniyedir.
+3. Start ve stop metotları ile loading başlatılıp durdurulabilir.
+
+| attr          | desc                                            |
+| :------------ | :-----------------------------------------------|
+| id            | Loading elementinin id'si                       |
+| timer         | Loading süresi (saniye)                         |
 '''
 
 code = """
-import uix
-import uix_components
-
 from uix.elements import button, col
 from uix_components import basic_loading
 
 def loading_example():
     with col() as content:
-        loading = basic_loading(id = "myLoading").style("height","400px")
+        loading = basic_loading(id = "myLoading", timer = 5).style("height","400px")
         button("Start Loading", id = "show_loading").on("click", lambda ctx, id, value : loading.start())
         button("Stop Loading", id = "hide_loading").on("click", lambda ctx, id, value : loading.stop())
     return content
